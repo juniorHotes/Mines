c6a83d9e1836a3b249db05d5dd761c76cdbda3ab
juniorHotes <juniorhotes53@gmail.com>
2020-11-07 00:07:10 -0300

    Testing field component.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, } from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
+
 
 export default class App extends Component {
   render() {
@@ -11,6 +13,15 @@ export default class App extends Component {
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColunmsAmount()}
         </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded/>
       </View>
     )
   }
